451c8d2d8a94c89503f54131b9f5909621a1966a
ArsRaf <59507652+ArsRaf@users.noreply.github.com>
2020-01-09 14:47:38 +0300

    Update JarTest1.py

--- JarTest1.py
+++ JarTest1.py
@@ -18,52 +18,13 @@ while True:
 	comparision = {
 	"compare_time" : fuzz.token_set_ratio(query.lower(), "time"),
 	"compare_date" : fuzz.token_set_ratio(query.lower(), "date"),
-	"compare_goodbye" : fuzz.token_set_ratio(query.lower(), "goodbye"),
-	"compare_music" : {
-		"jackpot" : fuzz.token_set_ratio(query.lower(), "jackpot"),
-		"toss_to_the_witcher" : fuzz.token_set_ratio(query.lower(), "toss to the witcher"),
-		"falling" : fuzz.token_set_ratio(query.lower(), "falling"),
-		"look_at_me_now" : fuzz.token_set_ratio(query.lower(), "look at me now"),
-		"roses" : fuzz.token_set_ratio(query.lower(), "roses"),
-		"hills" : fuzz.token_set_ratio(query.lower(), "hills"),
-		"immigrant_song" : fuzz.token_set_ratio(query.lower(), "immigrant song"),
-		"back_in_black" : fuzz.token_set_ratio(query.lower(), "back in black"),
-		"old_town_road" : fuzz.token_set_ratio(query.lower(), "old town road")
-		},
-	"compare_telegram" : fuzz.token_set_ratio(query.lower(), "telegram")
+	"compare_goodbye" : fuzz.token_set_ratio(query.lower(), "goodbye")
 	}
 	if comparision["compare_time"] > 75:
 		speak_engine.say("Now " + time.strftime("%H:%M", time.localtime()))
 		speak_engine.runAndWait()
 	elif comparision["compare_date"] > 75:
 		speak_engine.say("Today " + time.strftime("%d of %b", time.localtime()))
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["jackpot"] > 75:
-		os.system("/music/jackpot.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["toss_to_the_witcher"] > 75:
-		os.system("/music/toss_to_the_witcher.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["falling"] > 75:
-		os.system("/music/falling.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["look_at_me_now"] > 75:
-		os.system("/music/look_at_me_now.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["roses"] > 75:
-		os.system("/music/roses.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["hills"] > 75:
-		os.system("/music/hills.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["immigrant_song"] > 75:
-		os.system("/music/immigrant_song.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["back_in_black"] > 75:
-		os.system("/music/back_in_black.mp3")
-		speak_engine.runAndWait()
-	elif comparision["compare_music"]["old_town_road"] > 75:
-		os.system("/music/old_town_road.mp3")
 		speak_engine.runAndWait()
 	elif comparision["compare_goodbye"] > 75:
 		speak_engine.say("Goodbye, sir!")
